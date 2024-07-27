26000b804b0c0cca6493cb2486fe81d580159af8
qb114514 <phao0724@163.com>
2024-07-27 15:08:23 +0800

    feat(history): Implement history

--- src/history.rs
+++ src/history.rs
@@ -6,24 +6,36 @@ pub struct History {
 
 impl History {
     pub fn new(current: String) -> Self {
-        todo!()
+        Self {current, backward_list: vec![], forward_list: vec![]}
     }
     pub fn current(&self) -> &String {
-        todo!()
+        &self.current
     }
     pub fn backward(&mut self) -> Result<(), ()> {
-        todo!()
+        if self.backward_list.is_empty() {
+            return Err(());
+        }
+        self.forward_list.push(self.current.clone());
+        self.current=self.backward_list.pop().unwrap();
+        Ok(())
     }
     pub fn forward(&mut self) -> Result<(), ()> {
-        todo!()
+        if self.forward_list.is_empty() {
+            return Err(());
+        }
+        self.backward_list.push(self.current.clone());
+        self.current=self.forward_list.pop().unwrap();
+        Ok(())
     }
     pub fn can_backward(&self) -> bool {
-        todo!()
+        !self.backward_list.is_empty()
     }
     pub fn can_forward(&self) -> bool {
-        todo!()
+        !self.forward_list.is_empty()
     }
     pub fn goto(&mut self, new_url: String) {
-        todo!()
+        self.backward_list.push(self.current.clone());
+        self.current=new_url;
+        self.forward_list.clear();
     }
 }
