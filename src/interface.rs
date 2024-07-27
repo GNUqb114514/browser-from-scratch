8673700762cbff707e562de298074850f0943c19
qb114514 <phao0724@163.com>
2024-07-27 15:25:48 +0800

    feat(interface): Migrate history into naivigation bar

--- src/interface.rs
+++ src/interface.rs
@@ -1,17 +1,26 @@
-use crate::consts;
+use crate::{consts, history::History};
 
 use iced::{
-    widget::{Column, Row, TextInput},
-    Sandbox,
+    widget::{Button, Column, Row, Text, TextInput}, Sandbox
 };
 
 pub struct Browser {
     url_field: String,
+    history: History
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     UrlChanged(String),
+    BackwardRequested,
+    ForwardRequested,
+    UrlSubmitted,
+}
+
+impl Browser {
+    fn drop_url_field(&mut self) {
+        self.url_field = self.history.current().clone();
+    }
 }
 
 impl Sandbox for Browser {
@@ -20,11 +29,12 @@ impl Sandbox for Browser {
     fn new() -> Self {
         Self {
             url_field: "about:new".to_owned(),
+            history: History::new("about:new".to_owned()),
         }
     }
 
     fn title(&self) -> String {
-        format!("{} - Fiser", self.url_field)
+        format!("{} - Fiser", self.history.current())
     }
 
     fn update(&mut self, message: Self::Message) {
@@ -32,6 +42,17 @@ impl Sandbox for Browser {
             Message::UrlChanged(new_url) => {
                 self.url_field = new_url;
             }
+            Message::BackwardRequested => {
+                self.history.backward().unwrap();
+                self.drop_url_field();
+            },
+            Message::ForwardRequested => {
+                self.history.forward().unwrap();
+                self.drop_url_field();
+            },
+            Message::UrlSubmitted => {
+                self.history.goto(self.url_field.clone());
+            },
         }
     }
 
@@ -40,9 +61,16 @@ impl Sandbox for Browser {
             .padding(consts::PADDING)
             .spacing(consts::PADDING)
             .push(
-                Row::new().spacing(consts::PADDING).push(
-                    TextInput::new("URL here", &self.url_field).on_input(Message::UrlChanged)
-                ),
+                Row::new().spacing(consts::PADDING)
+                    .push(
+                        Button::new(Text::new("<")).on_press_maybe(if self.history.can_backward() {Some(Message::BackwardRequested)} else {None})
+                    )
+                    .push(
+                        Button::new(Text::new(">")).on_press_maybe(if self.history.can_forward() {Some(Message::ForwardRequested)} else {None})
+                    )
+                    .push(
+                        TextInput::new("URL here", &self.url_field).on_input(Message::UrlChanged).on_submit(Message::UrlSubmitted)
+                    ),
             )
             .into()
     }
